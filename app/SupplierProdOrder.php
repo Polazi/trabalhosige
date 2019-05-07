21483eef659ffb2f475a5033eec6d37aee7d234f
Vinicius Polazi <vinicius.polazi@gmail.com>
2019-05-07 20:10:52 -0300

    Comit em sala de aula

---
+++ app/SupplierProdOrder.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierProdOrder extends Model
+{
+    //
+}
---
+++ database/migrations/2019_05_05_181906_create_franchise_prod_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFranchiseProdOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('franchise_prod_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('franchise_prod_orders');
+    }
+}
